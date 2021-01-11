31fc835875ba1d269e026f1a6e67626c0d02665d
Omar Torres <torres.omar@berkeley.edu>
2021-01-10 21:11:26 -0800

    commiting final version

--- js/header.js
+++ js/header.js
@@ -87,7 +87,7 @@ function goToAboutMe() {
 }  
 
 function goToResume() {
-  window.location.href = "https://drive.google.com/file/d/1F20G7Ghw7M6QJJRPkpFGg1jfNBAnb0du/view?usp=sharing";
+  window.location.href = "https://drive.google.com/file/d/1wPLh1uTHtyD8LtY22kVAHTw7U4Q8vtgq/view?usp=sharing";
 } 
 
 ////////////////////////////////scrolling to protfolio///////////////////
